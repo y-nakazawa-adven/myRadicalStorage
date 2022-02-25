3c6cfe526b6138cd517976330b5e8a4e7245d472
寺島 昭平 <s-terashima@adventure-inc.co.jp>
2022-02-25 19:33:32 +0900

    feat(front): param to query for search

--- frontend/features/searchBar/libs/searchInput.ts
+++ frontend/features/searchBar/libs/searchInput.ts
@@ -1,23 +1,6 @@
 import { SearchInput } from '@features/searchBar'
-import { isNumber, isString } from '@libs/utils/common'
+import { isNumber, isString, removeColon, removeSlash } from '@libs/utils/common'
 import { HHmmToTimeFormat, yyyyMMddToSlash } from 'libs/utils/day'
-
-// const d = new Date()
-// export const defaultSearchInput: SearchInput = {
-//   objectID: '',
-//   location: '',
-//   address: '',
-//   _geoloc: {
-//     lat: 0,
-//     lng: 0,
-//   },
-//   checkinDay: format(new Date(), 'yyyy/MM/dd'),
-//   checkinTime: format(new Date(), 'HH:mm'),
-//   checkoutDay: format(addDays(new Date(), 1), 'yyyy/MM/dd'),
-//   checkoutTime: format(addDays(new Date(), 1), 'HH:mm'),
-//   countSizeA: 0,
-//   countSizeB: 0,
-// }
 
 export const queryToParam = (query: any): SearchInput => ({
   objectID: '',
@@ -34,3 +17,30 @@ export const queryToParam = (query: any): SearchInput => ({
   countSizeA: !!query.countSizeA && isNumber(query.countSizeA) ? query.countSizeA : 0,
   countSizeB: !!query.countSizeB && isNumber(query.countSizeB) ? query.countSizeB : 0,
 })
+
+export const paramToQuery = (param: SearchInput): { [key: string]: string } => {
+  const query: { [key: string]: string } = {}
+  if (param.location !== '') {
+    query.location = param.location
+  }
+  if (param.address !== '') {
+    query.address = param.address
+  }
+  if (param._geoloc.lat !== 0) {
+    query.lat = param._geoloc.lat as unknown as string
+  }
+  if (param._geoloc.lng !== 0) {
+    query.lng = param._geoloc.lng as unknown as string
+  }
+  query.checkinDay = removeSlash(param.checkinDay)
+  query.checkinTime = removeColon(param.checkinTime)
+  query.checkoutDay = removeSlash(param.checkoutDay)
+  query.checkoutTime = removeColon(param.checkoutTime)
+  if (param.countSizeA > 0) {
+    query.countSizeA = param.countSizeA as unknown as string
+  }
+  if (param.countSizeB > 0) {
+    query.countSizeB = param.countSizeB as unknown as string
+  }
+  return query
+}
--- frontend/libs/utils/common.ts
+++ frontend/libs/utils/common.ts
@@ -1,3 +1,6 @@
 export const isString = (s: any): s is string => typeof s === 'string'
 
 export const isNumber = (n: any): n is number => !isNaN(Number(n)) && n > 0
+
+export const removeSlash = (s: string): string => s.split('/').join()
+export const removeColon = (s: string): string => s.split(':').join()
